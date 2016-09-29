c98aa6430bd9f2ccdc4fff13b3907ce872ad45a4
Samuel Zhang <syzhang88@users.noreply.github.com>
2016-09-28 18:32:39 -0700

    Add files via upload
    added filter function hw1

---
+++ mpi_filter.py
@@ -0,0 +1,29 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 0:
+    data = [1,2,3,4,5]
+else:
+    data = []
+
+#Filter Function
+def filter_condition(x):
+    return x % 2 == 0
+
+#Scatter
+data = comm.scatter(data, root=0)
+print "rank", rank, "recieved", data
+data = data + 2
+
+#Adding Filter
+data = (filter_condition(data), data)
+
+#Gather
+data = comm.gather(data, root=0)
+
+comm.Barrier()
+
+if rank == 0:
+    print data
